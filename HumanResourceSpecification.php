0d15aa4d0f005f1a05d439813ab9cc2b5f7b6c52
roofimon <roofimon@gmail.com>
2014-01-17 12:05:51 +0700

    Finish Case: 23 is friday

--- HumanResourceSpecification.php
+++ HumanResourceSpecification.php
@@ -1,12 +1,18 @@
 <?php
 class HumanResource{
 	public $date;
-	function __construct($date){
+	public $weekDay;
+	function __construct($date,$weekDay=false){
 		$this->date=$date;
+		if(false !== $weekDay) {
+			$this->weekDay = $weekDay;	
+		}
 	}
 	function salaryIsPaid(){
 		if($this->date==25){
 			return "You are rich man";	
+		}else if($this->date == 23 && $this->weekDay == "Friday") {
+			return "22 days left";
 		}
 		return "Tomorrow dude";
 	}
@@ -25,6 +31,15 @@ class HumanResourceSpecification extends PHPUnit_Framework_TestCase {
 		$expected = "Tomorrow dude";
 		$date=24;
 		$humanResource = new HumanResource($date);
+		$actual = $humanResource->salaryIsPaid();
+		$this->assertEquals($expected, $actual);
+	}
+	
+	function test23isFriday(){
+		$expected = "22 days left";
+		$date=23;
+		$weekDay = "Friday";
+		$humanResource = new HumanResource($date, $weekDay);
 		$actual = $humanResource->salaryIsPaid();
 		$this->assertEquals($expected, $actual);
 	}
