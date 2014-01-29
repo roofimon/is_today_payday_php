f171b083b65bddf48c72ed56f20062baeb121f12
Pitsanu Swangpheaw <pitsanu_s@hotmail.com>
2014-01-29 13:22:56 +0700

    reformat

--- HumanResourceSpecification.php
+++ HumanResourceSpecification.php
@@ -1,14 +1,15 @@
 <?php
 require_once 'Payday.php';
 class HumanResource{
-	public $currentDate;
+  public $currentDate;
   public $payday;
-	function __construct(Payday $payday, DateTime $currentDate){
+
+  function __construct(Payday $payday, DateTime $currentDate){
     $this->payday = $payday;
-		$this->currentDate = $currentDate;
-	}
-	
-	function salaryIsPaid(){
+    $this->currentDate = $currentDate;
+  }
+
+  function salaryIsPaid(){
     $currentDate = $this->currentDate->format('j');
     $actualPaydate = $this->payday->getDate();
     $actualPayday = $this->payday->getDay();
@@ -25,57 +26,58 @@ class HumanResource{
 }
 
 class HumanResourceSpecification extends PHPUnit_Framework_TestCase {
-	function testTodayIsPayDay(){
-		$expected = "You are rich man";
+  function testTodayIsPayDay(){
+    $expected = "You are rich man";
     $payday = new Payday(new DateTime(2014-8-25));
-	  $mockCurrentDate = $this->getMock('DateTime', array('format'));
-		$mockCurrentDate->expects($this->once())
-		                 ->method('format')
-										 ->with($this->equalTo('j'))
-										 ->will($this->returnValue(25));
+    $mockCurrentDate = $this->getMock('DateTime', array('format'));
+    $mockCurrentDate->expects($this->once())
+                    ->method('format')
+                    ->with($this->equalTo('j'))
+                    ->will($this->returnValue(25));
 
-		$humanResource = new HumanResource($payday, $mockCurrentDate);
-		$actual = $humanResource->salaryIsPaid();
-		$this->assertEquals($expected, $actual);
-	}
+    $humanResource = new HumanResource($payday, $mockCurrentDate);
+    $actual = $humanResource->salaryIsPaid();
+    $this->assertEquals($expected, $actual);
+  }
 
-	function testTomorrowIsPayDay(){
-		$expected = "Tomorrow dude";
+  function testTomorrowIsPayDay(){
+    $expected = "Tomorrow dude";
     $payday = new Payday(new DateTime(2014-8-25));
-	  $mockCurrentDate = $this->getMock('DateTime', array('format'));
-		$mockCurrentDate->expects($this->once())
-		                 ->method('format')
-										 ->with($this->equalTo('j'))
-										 ->will($this->returnValue(24));
+    $mockCurrentDate = $this->getMock('DateTime', array('format'));
+    $mockCurrentDate->expects($this->once())
+                    ->method('format')
+                    ->with($this->equalTo('j'))
+                    ->will($this->returnValue(24));
 
-		$humanResource = new HumanResource($payday, $mockCurrentDate);
-		$actual = $humanResource->salaryIsPaid();
-		$this->assertEquals($expected, $actual);
-	}
-	
-	function test22DaysLeft(){
-		$expected = "22 days left and Thursday 25 is payday";
-	  $mockCurrentDate = $this->getMock('DateTime', array('format'));
+    $humanResource = new HumanResource($payday, $mockCurrentDate);
+    $actual = $humanResource->salaryIsPaid();
+    $this->assertEquals($expected, $actual);
+  }
+
+  function test22DaysLeft(){
+    $expected = "22 days left and Thursday 25 is payday";
+    $mockCurrentDate = $this->getMock('DateTime', array('format'));
     $payday = new Payday(new DateTime(2014-8-25));
-		$mockCurrentDate->expects($this->any())
-		                 ->method('format')									 
-                     ->with($this->equalTo('j'))
-										 ->will($this->returnValue("3"));
-		$humanResource = new HumanResource($payday, $mockCurrentDate);
-		$actual = $humanResource->salaryIsPaid();
-		$this->assertEquals($expected, $actual);
-	}
+    $mockCurrentDate->expects($this->any())
+                    ->method('format')
+                    ->with($this->equalTo('j'))
+                    ->will($this->returnValue("3"));
+    $humanResource = new HumanResource($payday, $mockCurrentDate);
+    $actual = $humanResource->salaryIsPaid();
+    $this->assertEquals($expected, $actual);
+  }
 
   function test3DaysLeftAndFridayIsPayday(){
-		$expected = "3 days left and Friday 23 is payday";
-	  $mockCurrentDate = $this->getMock('DateTime', array('format'));
+    $expected = "3 days left and Friday 23 is payday";
+    $mockCurrentDate = $this->getMock('DateTime', array('format'));
     $payday = new Payday(new DateTime(2014-5-25));
-		$mockCurrentDate->expects($this->any())
-		                 ->method('format')									 
-                     ->with($this->equalTo('j'))
-										 ->will($this->returnValue("20"));
-		$humanResource = new HumanResource($payday, $mockCurrentDate);
-		$actual = $humanResource->salaryIsPaid();
-		$this->assertEquals($expected, $actual);
+    $mockCurrentDate->expects($this->any())
+                    ->method('format')
+                    ->with($this->equalTo('j'))
+                    ->will($this->returnValue("20"));
+    $humanResource = new HumanResource($payday, $mockCurrentDate);
+    $actual = $humanResource->salaryIsPaid();
+    $this->assertEquals($expected, $actual);
   }
+
 }
